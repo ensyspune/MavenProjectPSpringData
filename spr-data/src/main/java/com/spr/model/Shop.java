e07d9dc3492d6942e4b3d54aba23ad00d0a5c863
Mahesh <mstambe.2009@gmail.com>
2015-11-23 17:01:18 +0530

    Spring Jpa

--- spr-data/src/main/java/com/spr/model/Shop.java
+++ spr-data/src/main/java/com/spr/model/Shop.java
@@ -1,10 +1,13 @@
 package com.spr.model;
 
+
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "shops")
